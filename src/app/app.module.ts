e676eed011dbd2581350cef42c317c28963b2353
Dor Bezalel <dor2081@gmail.com>
2023-05-08 12:52:02 +0300

    add AuthModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,28 +11,25 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
-import { RouterModule } from '@angular/router';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HeaderComponent,
-		AuthComponent,
 	],
 	imports: [
 		BrowserModule,
-		FormsModule,
-		ReactiveFormsModule,
 		HttpClientModule,
 		AppRoutingModule,
 		RecipesModule,
-		RouterModule,
 		ShoppingListModule,
 		SharedModule,
 		CoreModule,
+    AuthModule
 	],
 	bootstrap: [AppComponent],
 })
